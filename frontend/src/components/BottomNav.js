34922d6c86e87bfe1a1b13b0a54ebc74fd5f658b
JYP-UJU <on.jypark@gmail.com>
2026-04-19 16:58:13 +0100

    수정

--- frontend/src/components/BottomNav.js
+++ frontend/src/components/BottomNav.js
@@ -18,8 +18,10 @@ function BottomNav() {
         <nav style={{
             position: 'fixed',
             bottom: 0,
-            left: 0,
-            right: 0,
+            left: '50%',
+            transform: 'translateX(-50%)',
+            width: '100%',
+            maxWidth: '600px',
             background: 'white',
             display: 'flex',
             justifyContent: 'space-around',
